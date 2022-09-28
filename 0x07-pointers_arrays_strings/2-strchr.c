1fafa1d2347e72ab131c4a23b633b8eea1cc3062
Motsoane <thopemotsoane@gmail.com>
2022-09-28 01:38:48 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ *  _strchr - Entry point
+ *  @s: input
+ *  @c: input
+ *  Return: Always 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+
+{
+
+	int k -0;
+
+	while (s[k] != '\0')
+
+	{
+
+		k++;
+
+		if (s[k] == c)
+
+		{
+
+			return (&s[k]);
+
+		}
+	}
+
+	retrurn ('\0');
+}
